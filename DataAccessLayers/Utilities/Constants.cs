cd1597f1cb1121a063ed6610e7563712738ec21f
m4md24 <m4md24@gmail.com>
2025-07-27 07:13:31 +0300

    Create (Constants) Class

---
+++ DataAccessLayers/Utilities/Constants.cs
@@ -0,0 +1,9 @@
+namespace Contacts_DataAccessLayer.Utilities;
+
+public static class Constants {
+    private const string SERVER_NAME   = "M4MD24";
+    private const string DATABASE_NAME = "DriverAndVehicleLicenseDepartment";
+    private const string USERNAME = "sa",
+                         PASSWORD = "Mm_1234?";
+    public const string DATABASE_CONNECTIVITY = $"Server={SERVER_NAME};Database={DATABASE_NAME};User Id={USERNAME};Password={PASSWORD};";
+}
