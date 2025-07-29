d02056a21396aec58121fc3e453ac05d15a6130b
m4md24 <m4md24@gmail.com>
2025-07-29 10:31:33 +0300

    Create (saveData) Method for (FullNames) Table

--- DataAccessLayers/AccountManagementSystem/src/FullNames.cs
+++ DataAccessLayers/AccountManagementSystem/src/FullNames.cs
@@ -6,6 +6,58 @@ using AccountManagementSystem.Utilities;
 namespace AccountManagementSystem;
 
 public class FullNames {
+    private static int saveData(
+        ref FullName   fullName,
+        string         query,
+        Constants.Mode mode
+    ) {
+        SqlConnection sqlConnection = new SqlConnection(
+            Constants.DATABASE_CONNECTIVITY
+        );
+
+        SqlCommand sqlCommand = new SqlCommand(
+            query,
+            sqlConnection
+        );
+
+        if (mode == Constants.Mode.Update)
+            sqlCommand.Parameters.AddWithValue(
+                "@fullNameID",
+                fullName.fullNameID
+            );
+
+        sqlCommand.Parameters.AddWithValue(
+            "@firstName",
+            fullName.firstName
+        );
+        sqlCommand.Parameters.AddWithValue(
+            "@secondName",
+            fullName.secondName
+        );
+        sqlCommand.Parameters.AddWithValue(
+            "@thirdName",
+            fullName.thirdName
+        );
+        sqlCommand.Parameters.AddWithValue(
+            "@fourthName",
+            fullName.fourthName
+        );
+
+        int rowAffected = 0;
+        try {
+            sqlConnection.Open();
+            rowAffected = sqlCommand.ExecuteNonQuery();
+        } catch (Exception exception) {
+            Console.WriteLine(
+                exception.Message
+            );
+        } finally {
+            sqlConnection.Close();
+        }
+
+        return rowAffected;
+    }
+
     public static FullName? getFullNameByFullNameID(
         ref int fullNameID
     ) {
