f13b4fe25e9e3a019e1c9fa91e09ab34836f04f6
m4md24 <m4md24@gmail.com>
2025-07-29 21:11:35 +0300

    Create (getAllCountries) Method for (Countries) Table

--- DataAccessLayers/DriverAndVehicleLicenseDepartment/src/Countries.cs
+++ DataAccessLayers/DriverAndVehicleLicenseDepartment/src/Countries.cs
@@ -7,6 +7,52 @@ using DriverAndVehicleLicenseDepartment.Utilities;
 namespace DriverAndVehicleLicenseDepartment;
 
 public class Countries {
+    public static List<Country>? getAllCountries() {
+        SqlConnection sqlConnection = new SqlConnection(
+            Constants.DATABASE_CONNECTIVITY
+        );
+        const string GET_ALL_COUNTRIES = """
+                                         USE DriverAndVehicleLicenseDepartment
+                                         SELECT *
+                                         FROM DriverAndVehicleLicenseDepartment.Countries
+                                         """;
+        SqlCommand sqlCommand = new SqlCommand(
+            GET_ALL_COUNTRIES,
+            sqlConnection
+        );
+
+        try {
+            sqlConnection.Open();
+            List<Country> countries     = [];
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            while (sqlDataReader.Read()) {
+                byte   countryID   = (byte) sqlDataReader["CountryID"];
+                string countryName = (string) sqlDataReader["CountryName"];
+                string countryCode = (string) sqlDataReader["CountryCode"];
+
+                countries.Add(
+                    new Country(
+                        countryID,
+                        countryName,
+                        countryCode
+                    )
+                );
+            }
+
+            sqlDataReader.Close();
+            return countries;
+        } catch (Exception exception) {
+            Console.WriteLine(
+                exception.Message
+            );
+        } finally {
+            sqlConnection.Close();
+        }
+
+        return null;
+    }
+
     public static Country? getCountryByCountryID(
         ref byte countryID
     ) {
