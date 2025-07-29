61022f37a59e6b83237b8beb959557173aa8853d
m4md24 <m4md24@gmail.com>
2025-07-29 09:33:44 +0300

    Create (setActiveStatus) Method for (Accounts) Table

--- DataAccessLayers/AccountManagementSystem/src/Accounts.cs
+++ DataAccessLayers/AccountManagementSystem/src/Accounts.cs
@@ -6,6 +6,50 @@ using AccountManagementSystem.Utilities;
 namespace AccountManagementSystem;
 
 public class Accounts {
+    public static int setActiveStatus(
+        ref int  accountID,
+        ref bool isActive
+    ) {
+        SqlConnection sqlConnection = new SqlConnection(
+            Constants.DATABASE_CONNECTIVITY
+        );
+
+        const string SET_ACTIVE_STATUS = """
+                                         USE DriverAndVehicleLicenseDepartment
+                                         UPDATE AccountManagementSystem.Accounts
+                                         SET IsActive = @isActive
+                                         WHERE AccountID = @accountID
+                                         """;
+
+        SqlCommand sqlCommand = new SqlCommand(
+            SET_ACTIVE_STATUS,
+            sqlConnection
+        );
+
+        sqlCommand.Parameters.AddWithValue(
+            "@isActive",
+            isActive
+        );
+        sqlCommand.Parameters.AddWithValue(
+            "@accountID",
+            accountID
+        );
+
+        int rowAffected = 0;
+        try {
+            sqlConnection.Open();
+            rowAffected = sqlCommand.ExecuteNonQuery();
+        } catch (Exception exception) {
+            Console.WriteLine(
+                exception.Message
+            );
+        } finally {
+            sqlConnection.Close();
+        }
+
+        return rowAffected;
+    }
+
     public static Account? getAccountByAccountID(
         ref int accountID
     ) {
