57b00d349be5dd14a69fdafe9db8eab6bcbbf494
m4md24 <m4md24@gmail.com>
2025-07-29 13:01:18 +0300

    Create (deleteCountryByCountryID) Method for (Countries) Table

--- DataAccessLayers/AccountManagementSystem/src/Countries.cs
+++ DataAccessLayers/AccountManagementSystem/src/Countries.cs
@@ -7,6 +7,41 @@ using AccountManagementSystem.Utilities;
 namespace AccountManagementSystem;
 
 public class Countries {
+    public static int deleteCountryByCountryID(
+        ref int countryID
+    ) {
+        SqlConnection sqlConnection = new SqlConnection(
+            Constants.DATABASE_CONNECTIVITY
+        );
+        const string DELETE_COUNTRY_BY_COUNTRY_ID = """
+                                                    USE DriverAndVehicleLicenseDepartment
+                                                    DELETE AccountManagementSystem.Countries
+                                                    WHERE CountryID = @countryID
+                                                    """;
+        SqlCommand sqlCommand = new SqlCommand(
+            DELETE_COUNTRY_BY_COUNTRY_ID,
+            sqlConnection
+        );
+        sqlCommand.Parameters.AddWithValue(
+            "@countryID",
+            countryID
+        );
+
+        int rowAffected = 0;
+        try {
+            sqlConnection.Open();
+            rowAffected = sqlCommand.ExecuteNonQuery();
+        } catch (Exception exception) {
+            Console.WriteLine(
+                exception.Message
+            );
+        } finally {
+            sqlConnection.Close();
+        }
+
+        return rowAffected;
+    }
+
     public static List<Country>? getAllCountries() {
         SqlConnection sqlConnection = new SqlConnection(
             Constants.DATABASE_CONNECTIVITY
