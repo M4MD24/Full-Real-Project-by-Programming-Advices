6518ba7b1d0d7c491a2bc5c3dc1c6de82fbfb24d
m4md24 <m4md24@gmail.com>
2025-07-30 14:03:47 +0300

    Create CRUDS Methods for (Countries)

---
+++ BusinessLayers/ClientManagementSystem-ClassLibrary-BusinessLayer/src/Countries.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using ClientManagementSystem_ClassLibrary_DataAccessLayer.Models;
+
+namespace ClientManagementSystem_ClassLibrary_BusinessLayer;
+
+public class Countries {
+    public static int update(
+        ref Country country
+    ) => ClientManagementSystem_ClassLibrary_DataAccessLayer.Countries.updateCountryByCountryID(
+        ref country
+    );
+
+    public static int delete(
+        ref byte countryID
+    ) => ClientManagementSystem_ClassLibrary_DataAccessLayer.Countries.deleteCountryByCountryID(
+        ref countryID
+    );
+
+    public static int add(
+        ref Country country
+    ) => ClientManagementSystem_ClassLibrary_DataAccessLayer.Countries.addNewCountry(
+        ref country
+    );
+
+    public static List<Country>? all() => ClientManagementSystem_ClassLibrary_DataAccessLayer.Countries.getAllCountries();
+
+    public static Country? get(
+        ref byte countryID
+    ) => ClientManagementSystem_ClassLibrary_DataAccessLayer.Countries.getCountryByCountryID(
+        ref countryID
+    );
+}
