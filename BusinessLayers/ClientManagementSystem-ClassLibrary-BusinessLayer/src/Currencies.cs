0faac35bdd928f2dcfe2fd0169eb8e427cff2fc8
m4md24 <m4md24@gmail.com>
2025-07-30 14:32:42 +0300

    Create CRUDS Methods for (Currencies)

---
+++ BusinessLayers/ClientManagementSystem-ClassLibrary-BusinessLayer/src/Currencies.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using ClientManagementSystem_ClassLibrary_DataAccessLayer.Models;
+
+namespace ClientManagementSystem_ClassLibrary_BusinessLayer;
+
+public class Currencies {
+    public static int update(
+        ref Currency currency
+    ) => ClientManagementSystem_ClassLibrary_DataAccessLayer.Currencies.updateCurrencyByCurrencyID(
+        ref currency
+    );
+
+    public static int delete(
+        ref byte currencyID
+    ) => ClientManagementSystem_ClassLibrary_DataAccessLayer.Currencies.deleteCurrencyByCurrencyID(
+        ref currencyID
+    );
+
+    public static int add(
+        ref Currency currency
+    ) => ClientManagementSystem_ClassLibrary_DataAccessLayer.Currencies.addNewCurrency(
+        ref currency
+    );
+
+    public static List<Currency>? all() => ClientManagementSystem_ClassLibrary_DataAccessLayer.Currencies.getAllCurrencies();
+
+    public static Currency? get(
+        ref byte currencyID
+    ) => ClientManagementSystem_ClassLibrary_DataAccessLayer.Currencies.getCurrencyByCurrencyID(
+        ref currencyID
+    );
+}
