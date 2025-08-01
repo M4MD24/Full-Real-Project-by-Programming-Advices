9c28e5bf1e3bfec4616158f66bffae0fd71dbbdd
m4md24 <m4md24@gmail.com>
2025-08-01 21:59:06 +0300

    Create CRUDS Methods for (Accounts)

--- BusinessLayers/AccountManagementSystem-ClassLibrary-BusinessLayer/src/Accounts.cs
+++ BusinessLayers/AccountManagementSystem-ClassLibrary-BusinessLayer/src/Accounts.cs
@@ -1,3 +1,32 @@
-﻿namespace AccountManagementSystem_ClassLibrary_BusinessLayer;
+﻿using System.Collections.Generic;
+using AccountManagementSystem_ClassLibrary_DataAccessLayer.Models;
 
-public class Accounts {}
+namespace AccountManagementSystem_ClassLibrary_BusinessLayer;
+
+public class Accounts {
+    public static int update(
+        ref Account account
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.Accounts.updateAccountByAccountID(
+        ref account
+    );
+
+    public static int delete(
+        ref int accountID
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.Accounts.deleteAccountByAccountID(
+        ref accountID
+    );
+
+    public static int add(
+        ref Account account
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.Accounts.addNewAccount(
+        ref account
+    );
+
+    public static List<Account>? all() => AccountManagementSystem_ClassLibrary_DataAccessLayer.Accounts.getAllAccounts();
+
+    public static Account? get(
+        ref int accountID
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.Accounts.getAccountByAccountID(
+        ref accountID
+    );
+}
