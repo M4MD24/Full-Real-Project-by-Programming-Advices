447e2ef6695c396a862925b3d72461a802d79d99
m4md24 <m4md24@gmail.com>
2025-08-01 22:30:09 +0300

    Create (CRUDS) Methods Without (Get All) Method for (ContactInformation)

--- BusinessLayers/AccountManagementSystem-ClassLibrary-BusinessLayer/src/ContactInformation.cs
+++ BusinessLayers/AccountManagementSystem-ClassLibrary-BusinessLayer/src/ContactInformation.cs
@@ -1,3 +1,27 @@
 ﻿namespace AccountManagementSystem_ClassLibrary_BusinessLayer;
 
-public class ContactInformation {}
+public class ContactInformation {
+    public static int update(
+        ref AccountManagementSystem_ClassLibrary_DataAccessLayer.Models.ContactInformation contactInformation
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.ContactInformation.updateContactInformationByContactInformationID(
+        ref contactInformation
+    );
+
+    public static int delete(
+        ref int contactInformationID
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.ContactInformation.deleteContactInformationByContactInformationID(
+        ref contactInformationID
+    );
+
+    public static int add(
+        ref AccountManagementSystem_ClassLibrary_DataAccessLayer.Models.ContactInformation contactInformation
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.ContactInformation.addNewContactInformation(
+        ref contactInformation
+    );
+
+    public static AccountManagementSystem_ClassLibrary_DataAccessLayer.Models.ContactInformation? get(
+        ref int contactInformationID
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.ContactInformation.getContactInformationByContactInformationID(
+        ref contactInformationID
+    );
+}
