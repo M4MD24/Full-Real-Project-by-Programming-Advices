f2a659027318cd37f8d56098c674afc0931e5675
m4md24 <m4md24@gmail.com>
2025-08-02 00:48:43 +0300

    Initialize (AccountManagementSystem-WindowsFormsApplication-PresentationLayer) Project

---
+++ PresentationLayers/AccountManagementSystem-WindowsFormsApplication-PresentationLayer/App.cs
@@ -0,0 +1,9 @@
+using System.Windows.Forms;
+
+namespace AccountManagementSystem_WindowsFormsApplication_PresentationLayer;
+
+public partial class App : Form {
+    public App() {
+        InitializeComponent();
+    }
+}
