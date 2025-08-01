7c09ee972279b52ca39c2fe669b0f3ff5e248a5e
m4md24 <m4md24@gmail.com>
2025-08-01 22:24:04 +0300

    Create (CRUDS) Methods Without (Get All) Method for (FullNames)

--- BusinessLayers/AccountManagementSystem-ClassLibrary-BusinessLayer/src/FullNames.cs
+++ BusinessLayers/AccountManagementSystem-ClassLibrary-BusinessLayer/src/FullNames.cs
@@ -1,3 +1,29 @@
-﻿namespace AccountManagementSystem_ClassLibrary_BusinessLayer;
+﻿using AccountManagementSystem_ClassLibrary_DataAccessLayer.Models;
 
-public class FullNames {}
+namespace AccountManagementSystem_ClassLibrary_BusinessLayer;
+
+public class FullNames {
+    public static int update(
+        ref FullName fullName
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.FullNames.updateFullNameByFullNameID(
+        ref fullName
+    );
+
+    public static int delete(
+        ref int fullNameID
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.FullNames.deleteFullNameByFullNameID(
+        ref fullNameID
+    );
+
+    public static int add(
+        ref FullName fullName
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.FullNames.addNewFullName(
+        ref fullName
+    );
+
+    public static FullName? get(
+        ref int fullNameID
+    ) => AccountManagementSystem_ClassLibrary_DataAccessLayer.FullNames.getFullNameByFullNameID(
+        ref fullNameID
+    );
+}
