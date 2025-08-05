e45d00cfd047e27b2a3edf7259ee969693ab2f51
m4md24 <m4md24@gmail.com>
2025-08-05 09:17:58 +0300

    Change DataOfBirth Type

--- RelationalSchemasAndDatabases/AccountManagementSystem/AccountManagementSystem.sql
+++ RelationalSchemasAndDatabases/AccountManagementSystem/AccountManagementSystem.sql
@@ -30,7 +30,7 @@ CREATE TABLE AccountManagementSystem.Persons
     PersonID             INT            NOT NULL PRIMARY KEY IDENTITY (1,1),
     NationalNumber       NVARCHAR(30)   NOT NULL UNIQUE,
     FullNameID           INT            NOT NULL UNIQUE,
-    DateOfBirth          DATETIME       NOT NULL,
+    DateOfBirth          DATE           NOT NULL,
     Address              NVARCHAR(200)  NOT NULL,
     ContactInformationID INT            NOT NULL,
     CountryID            TINYINT        NOT NULL,
